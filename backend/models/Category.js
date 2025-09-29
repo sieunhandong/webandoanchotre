ebab2c839f8900ee2dba037a04ff3c3efc96b1ab
sieunhandong <dongnvhe172730@fpt.edu.vn>
2025-09-29 19:28:02 +0700

    clean project

--- backend/models/Category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const categorySchema = new mongoose.Schema({
-    name: String,
-    description: String,
-}, { timestamps: false });
-
-module.exports = mongoose.model('Category', categorySchema);
--- backend/routes/discountRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const discountController = require("../controllers/DiscountController");
-
-
-router.get("/suitable", discountController.getDiscountSuitable);
-
-module.exports = router;
